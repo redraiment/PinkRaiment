d2dbf6d6aab8e8ebee21c7d203722ad06ffb453b
redraiment <redraiment@gmail.com>
2015-07-08 10:17:18 +0800

    同时下载phpActiveRecord

--- bin/get.pink.sh
+++ bin/get.pink.sh
@@ -2,6 +2,13 @@
 
 git clone --depth 1 http://git.oschina.net/redraiment/PinkRaiment.git
 cd PinkRaiment
+
+rm bin/get.pink.sh
+
+git clone --depth 1 http://git.oschina.net/redraiment/phpActiveRecord.git
+mv phpActiveRecord app/models
+rm -rf app/models/.git* app/models/*test* app/models/zombie.php
+
 echo 'bin/' > .gitignore
 rm -rf .git
 git init
